fc7169be9c2ac4eeede68b5dd3067fdb20063c1e
sgalal <sgalal.me@outlook.com>
2020-01-05 16:33:27 +0800

    Update UI

--- index.js
+++ index.js
@@ -1,53 +1,93 @@
 'use strict';
 
-async function handleSchemaClick(val) {
-	const response = await fetch('examples/' + val + '.js');
-	const text = await response.text();
-	codeInputArea.setValue(text);
-	loadSchema();
-}
+/* Global utilities */
 
-// Function that displays a pop-up alert
-function notify(text) {
+function notify(text) {  // Function that displays a pop-up dialog
 	Swal.fire({animation: false, text: text});
 }
 
-function makeErr(err, i) {
-	return '小韻 ' + i + ': ' + err.message + '\n' + err.stack;
+function notifyError(text) {  // Function that displays a pop-up alert
+	Swal.fire({animation: false, text: text});
 }
 
-var codeInputArea;
+function myFlat(arrays) {  // Equals to Array.prototype.flat(), but supports Edge
+	return arrays.reduce(function(a, b) { return a.concat(b); }, []);
+}
+
+/* Page initializer */
+
+let codeInputArea;
 
 document.addEventListener('DOMContentLoaded', () => {
-	// Initialize codemirror
+	/* Initialize codemirror */
 	codeInputArea = CodeMirror(schemaInput, {
 		value: 'const is = s => equal音韻地位(小韻號, s);\n\n// Your script goes here\n',
 		mode: 'javascript',
 		lineNumbers: true
 	});
 
-	// Load the selected schema
-	handleSchemaClick(document.forms.schemaSelect.schema.value);
+	/* Load the selected schema */
+	handleLoadSchema(document.forms.schemaSelect.schema.value);
 });
+
+/* Page event handler */
+
+function handleLoadSchema(val) {
+	fetch('examples/' + val + '.js')
+	.then(response => {
+		return response.text();
+	})
+	.then(text => {
+		codeInputArea.setValue(text);
+	})
+	.catch((error) => {
+		console.error('Error:', error);
+	});
+}
+
+function handlePredefinedOptions() {
+	loadSchema();
+	if (predefinedOptions.value == 'exportAllSmallRhymes') {
+		outputArea.innerText = [...Array(3874).keys()].map(i => get音韻描述(i + 1) + ' ' + brogue2(i + 1)).join('\n');
+		outputArea.handleExport = null;
+	} else if (predefinedOptions.value == 'exportAllSyllables') {
+		outputArea.innerText = [...new Set([...Array(3874).keys()].map(i => brogue2(i + 1)))].join(', ');
+		outputArea.handleExport = null;
+	} else if (predefinedOptions.value == 'convertArticle') {
+		handleArticle();
+	}
+}
+
+function handleCopy() {
+	const text = !outputArea.handleExport
+		? outputArea.innerText
+		: outputArea.handleExport();  // A user-defined attribute
+
+	navigator.clipboard.writeText(text).then(() => {
+		notify('已成功匯出至剪貼簿。');
+	}, () => {
+		notify('匯出至剪貼簿失敗。');
+	});
+}
 
 /* Brogue2 function */
 
-var brogue2_inner;
+let brogue2_inner;
 
 function loadSchema() {
 	try {
 		brogue2_inner = new Function('小韻號', '字頭', codeInputArea.getValue());
 	} catch (err) {
-		notify(makeErr(err));
+		notifyError(makeErr(err));
 	}
 }
 
 function brogue2(小韻號, 字頭) {
-	var res;
+	let res;
 	try {
 		res = brogue2_inner(小韻號, 字頭);
 	} catch (err) {
-		notify(makeErr(err, i + 1));
+		notifyError(makeErr(err, i + 1));
 		throw err;
 	}
 	if (res == null)
@@ -55,24 +95,39 @@ function brogue2(小韻號, 字頭) {
 	return res;
 }
 
-/* Predefined Options */
+/* Make conversion */
 
-function handlePredefinedOptions() {
-	if (predefinedOptions.value == 'exportAllSmallRhymes') {
-		loadSchema();
-		outputArea.innerText = [...Array(3874).keys()].map(i => get音韻描述(i + 1) + ' ' + brogue2(i + 1)).join('\n');
-		outputArea.handleExport = null;
-	} else if (predefinedOptions.value == 'exportAllSyllables') {
-		loadSchema();
-		outputArea.innerText = [...new Set([...Array(3874).keys()].map(i => brogue2(i + 1)))].join(', ');
-		outputArea.handleExport = null;
-	} else if (predefinedOptions.value == 'convertArticle') {
-		loadSchema();
-		handleArticle();
-	}
+function handleArticle() {
+	outputArea.innerHTML = '';
+
+	const convertText = articleInput.value;
+	[...convertText].map(n => {
+		outputArea.appendChild(makeConversion(n));
+	});
+	outputArea.handleExport = () => [...outputArea.childNodes].map(node => node.handleExport()).join('');
 }
 
-/* Converter */
+function makeErr(err, i) {
+	return '小韻 ' + i + ': ' + err.message + '\n' + err.stack;
+}
+
+function makeConversion(ch) {
+	const qiteis = (qitei[ch] || []).slice();  // 得到 ch 的所有異體字
+	qiteis.unshift(ch);  // Include ch itself
+	const res = myFlat(qiteis.map(ch => {
+		return query切韻音系(ch).map(o => {  // 查出 小韻號 和 解釋
+			o['字頭'] = ch;
+			return o;  // { 字頭, 小韻號, 解釋 }
+		});
+	}));
+
+	if (!res.length)
+		return makeNoneEntry(ch);
+	else if (res.length == 1)
+		return makeSingleEntry(ch, res[0]);
+	else
+		return makeMultipleEntry(ch, res);
+}
 
 function makeLongStr(sr) {
 	return get音韻描述(sr);
@@ -85,38 +140,60 @@ function makeTooltip(ch, pronunciation, sr, expl) {
 	return div;
 }
 
+/* Make entries */
+
 function makeNoneEntry(ch) {
+	/* Format for .entry-none example:
+	<div class="entry entry-none">。</div>
+	*/
 	const outerContainer = document.createElement('div');
 	outerContainer.classList.add('entry');
 	outerContainer.classList.add('entry-none');
 	outerContainer.appendChild(document.createTextNode(ch));
 	outerContainer.handleExport = () => ch;
-
 	return outerContainer;
 }
 
 function makeSingleEntry(ch, res) {
-	const [actual_ch, sr, expl] = res;
-	var pronunciation = brogue2(sr);
+	/* Format for .entry-single example:
+	<div class="entry entry-single">
+		<ruby>
+			年
+			<rp>(</rp>
+			<rt lang="zh-Latn-x-output">den˨˩</rt>
+			<rp>)</rp>
+		</ruby>
+		<div class="tooltip-container">
+			<div class="tooltip-item">年 <span lang="zh-Latn-x-output">nen</span> 泥開四先平 上同</div>
+		</div>
+	</div>
+	*/
+	const pronunciation = brogue2(res['小韻號']);
 
 	const outerContainer = document.createElement('div');
 	outerContainer.classList.add('entry');
 	outerContainer.classList.add('entry-single');
 
 	const ruby = document.createElement('ruby');
-	const rb = document.createElement('rb');
-	rb.appendChild(document.createTextNode(ch));
-	ruby.appendChild(rb);
+	ruby.appendChild(document.createTextNode(ch));
 
 	const tooltipContainer = document.createElement('div');
 	tooltipContainer.classList.add('tooltip-container');
+
+	const rp_left = document.createElement('rp');
+	rp_left.appendChild(document.createTextNode('('));
+	ruby.appendChild(rp_left);
 
 	const rt = document.createElement('rt');
 	rt.lang = 'zh-Latn-x-output';
 	rt.innerText = pronunciation;
 	ruby.appendChild(rt);
 
-	const tooltip = makeTooltip(actual_ch, pronunciation, sr, expl);
+	const rp_right = document.createElement('rp');
+	rp_right.appendChild(document.createTextNode(')'));
+	ruby.appendChild(rp_right);
+
+	const tooltip = makeTooltip(res['字頭'], pronunciation, res['小韻號'], res['解釋']);
 	tooltipContainer.appendChild(tooltip);
 
 	outerContainer.appendChild(ruby);
@@ -128,37 +205,59 @@ function makeSingleEntry(ch, res) {
 }
 
 function makeMultipleEntry(ch, ress) {
+	/* Format for .entry-multiple example:
+	<div class="entry entry-multiple unresolved">
+		<ruby>
+			盡
+			<rp>(</rp>
+			<rt lang="zh-Latn-x-output">
+				<span>sin˨˩</span>
+				<span class="hidden">sin˧˥</span>
+			</rt>
+			<rp>)</rp>
+		</ruby>
+		<div class="tooltip-container">
+			<div class="tooltip-item selected">盡 <span lang="zh-Latn-x-output">sin˨˩</span> 從開三眞上 竭也終也慈忍切又即忍切二</div>
+			<div class="tooltip-item">盡 <span lang="zh-Latn-x-output">sin˧˥</span> 精開三眞上 曲禮曰虛坐盡前[-/虚坐盡後虚坐盡前]又慈忍切</div>
+		</div>
+	</div>
+	*/
 	const outerContainer = document.createElement('div');
 	outerContainer.classList.add('entry');
 	outerContainer.classList.add('entry-multiple');
 	outerContainer.classList.add('unresolved');
 
 	const ruby = document.createElement('ruby');
-	const rb = document.createElement('rb');
-	rb.appendChild(document.createTextNode(ch));
-	ruby.appendChild(rb);
+	ruby.appendChild(document.createTextNode(ch));
 
 	const tooltipContainer = document.createElement('div');
 	tooltipContainer.classList.add('tooltip-container');
 
+	const rp_left = document.createElement('rp');
+	rp_left.appendChild(document.createTextNode('('));
+	ruby.appendChild(rp_left);
+
 	const rt = document.createElement('rt');
 	rt.lang = 'zh-Latn-x-output';
 	ruby.appendChild(rt);
+
+	const rp_right = document.createElement('rp');
+	rp_right.appendChild(document.createTextNode(')'));
+	ruby.appendChild(rp_right);
 
 	let rtSpanArray = [];
 	let tooltipArray = [];
 
 	for (let i = 0, len = ress.length; i < len; i++) {
 		const res = ress[i];
-		const [actual_ch, sr, expl] = res;
-		var pronunciation = brogue2(sr);
+		const pronunciation = brogue2(res['小韻號']);
 
 		const rtSpan = document.createElement('span');
 		rtSpan.innerText = pronunciation;
 		rt.appendChild(rtSpan);
 		rtSpanArray.push(rtSpan);
 
-		const tooltip = makeTooltip(actual_ch, pronunciation, sr, expl);
+		const tooltip = makeTooltip(res['字頭'], pronunciation, res['小韻號'], res['解釋']);
 		tooltip.addEventListener('click', () => {
 			rtSpanArray.map(rtSpan => rtSpan.classList.add('hidden'));
 			rtSpan.classList.remove('hidden');
@@ -185,40 +284,4 @@ function makeMultipleEntry(ch, ress) {
 	outerContainer.handleExport = () => ch + '(' + outerContainer.currentSelection + ')';
 
 	return outerContainer;
-}
-
-function makeConversion(c) {
-	// 異體字轉換
-	var qiteis = (qitei[c] || []).slice();
-	qiteis.unshift(c);
-	var res = qiteis.map(c => {
-		return query切韻音系(c).map(o => [c, o['小韻號'], o['解釋']]);
-	}).flat();
-
-	if (!res.length)
-		return makeNoneEntry(c);
-	else if (res.length == 1)
-		return makeSingleEntry(c, res[0]);
-	else
-		return makeMultipleEntry(c, res);
-}
-
-function handleArticle() {
-	outputArea.innerHTML = '';
-
-	const convertText = articleInput.value;
-	[...convertText].map(n => {
-		outputArea.appendChild(makeConversion(n));
-	});
-	outputArea.handleExport = () => [...outputArea.childNodes].map(node => node.handleExport()).join('');
-}
-
-function handleCopy() {
-	const text = !outputArea.handleExport ? outputArea.innerText : outputArea.handleExport();
-
-	navigator.clipboard.writeText(text).then(() => {
-		notify('已成功匯出至剪貼簿。');
-	}, () => {
-		notify('匯出至剪貼簿失敗。');
-	});
 }
