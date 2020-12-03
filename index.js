11f4e3ed778b2879c2b1167199bd482464ac6a72
sgalal <sgalal.me@outlook.com>
2020-12-03 10:39:46 +0800

    Add gwongzau

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Load schema to code area
 function handleLoadSchema(val) {
-	fetch('https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@20201106/' + val + '.js')
+	fetch('https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@8040075/' + val + '.js')
 	.then(response => response.text())
 	.then(txt => schemaInputArea.setValue(txt))
 	.catch(err => notifyError(err));
