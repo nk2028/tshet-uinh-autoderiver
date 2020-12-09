5b91ac4eba5e2461d9d506fb43345083eb4cf2cd
sgalal <sgalal.me@outlook.com>
2020-12-09 09:14:55 +0800

    Add unt_j (beta)

--- index.js
+++ index.js
@@ -102,10 +102,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Load schema to code area
 function handleLoadSchema(val) {
-	fetch('https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@91abb20/' + val + '.js')
-	.then(response => response.text())
-	.then(txt => schemaInputArea.setValue(txt))
-	.catch(err => notifyError(err));
+	if (val === 'unt_j') {
+		fetch('https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@6d379e8/unt_2020.js')
+		.then(response => response.text())
+		.then(txt => schemaInputArea.setValue(txt))
+		.catch(err => notifyError(err));
+	} else {
+		fetch('https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@91abb20/' + val + '.js')
+		.then(response => response.text())
+		.then(txt => schemaInputArea.setValue(txt))
+		.catch(err => notifyError(err));
+	}
 }
 
 function handlePredefinedOptions() {
