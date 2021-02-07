26440c017ee9a07b3f98611803d07f9f8ee7e825
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-02-07 20:25:42 +0800

    Add fanwan

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import LargeTooltip from 'large-tooltip';
 // load schema
 
 export function handleLoadSchema(schema) {
-  fetch(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@334c64a/${schema}.js`)
+  fetch(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@a949743/${schema}.js`)
   .then((response) => response.text())
   .then((txt) => schemaInputArea.setValue(txt))
   .catch((err) => notifyError(err));
