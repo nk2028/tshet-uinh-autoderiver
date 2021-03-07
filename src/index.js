2b1baf6306bac8909294e118fc83649bc6fb79a1
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-03-07 23:42:11 +0800

    Update qieyun-examples to c9ee09a

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import LargeTooltip from 'large-tooltip';
 // load schema
 
 export function handleLoadSchema(schema) {
-  fetch(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@0249093/${schema}.js`)
+  fetch(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@c9ee09a/${schema}.js`)
   .then((response) => response.text())
   .then((txt) => schemaInputArea.setValue(txt))
   .catch((err) => notifyError(err));
