ff66a7fffe6e586bc51fd11a0897318b2f5ef412
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-03-31 12:26:01 +0800

    Update qieyun-examples to ef431c2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import LargeTooltip from 'large-tooltip';
 // load schema
 
 export function handleLoadSchema(schema) {
-  fetch(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@4547468/${schema}.js`)
+  fetch(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@ef431c2/${schema}.js`)
   .then((response) => response.text())
   .then((txt) => schemaInputArea.setValue(txt))
   .catch((err) => notifyError(err));
