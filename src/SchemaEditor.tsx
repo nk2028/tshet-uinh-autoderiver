0819d0cee90032a3f9da06631b71114419d390e4
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-06-21 11:02:19 +0800

    Update qieyun-examples to 912a9bb

--- src/SchemaEditor.tsx
+++ src/SchemaEditor.tsx
@@ -85,7 +85,7 @@ class SchemaEditor extends React.Component<SchemaProps, any> {
 
   loadSchema(event?: any) {
     if (event) event.preventDefault();
-    fetchFile(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@08367a4/${this.props.name}.js`, input => {
+    fetchFile(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@912a9bb/${this.props.name}.js`, input => {
       if (event && this.props.input && this.props.input !== this.props.original) {
         Swal.fire({
           showClass: { popup: "" },
