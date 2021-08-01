accfd80d2f020992cba52a12bfdd23da0dbce5b5
unt <unt_tianjin@163.com>
2021-08-01 16:22:37 +0800

    Update qieyun-examples to e3dadfb

--- src/SchemaEditor.tsx
+++ src/SchemaEditor.tsx
@@ -85,7 +85,7 @@ class SchemaEditor extends React.Component<SchemaProps, any> {
 
   loadSchema(event?: any) {
     if (event) event.preventDefault();
-    fetchFile(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@33bce60/${this.props.name}.js`, input => {
+    fetchFile(`https://cdn.jsdelivr.net/gh/nk2028/qieyun-examples@e3dadfb/${this.props.name}.js`, input => {
       if (event && this.props.input && this.props.input !== this.props.original) {
         Swal.fire({
           showClass: { popup: "" },
