f5c9975bf46f64bb77796697e76daa13731d5d76
graphemecluster <graphemecluster@gmail.com>
2022-06-28 09:09:42 +0800

    Bug Fix

--- src/editor/types.d.ts
+++ src/editor/types.d.ts
@@ -8,7 +8,5 @@ declare global {
   const 音韻地位: 音韻地位;
   const 字頭: string | null;
   const 選項: Record<string, unknown>;
-  const is: 音韻地位["屬於"];
-  const when: 音韻地位["判斷"];
   const require: (sample: string) => UserSchema;
 }
