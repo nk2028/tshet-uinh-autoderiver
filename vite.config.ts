b67568273bfae501c63fe43be2b1e008b0bdf85b
syimyuzya <syimyuzya@gmail.com>
2024-08-07 14:26:05 +0800

    Fix: source map for Emotion CSS
    I was wrong about Emotion CSS "working out of the box" in the last
    commit. Emotion always work even installed without the Babel plugin, but
    what does not work is the source map, which requires the plugin.
    
    This also means that Vite's React plugin has to be the Babel version,
    not the other one with SWC.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@emotion"],
+      },
+    }),
+  ],
   base: "/qieyun-autoderiver/",
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
