1c9620903d882e6f6828140cfdc30c765b0e42f9
syimyuzya <syimyuzya@gmail.com>
2024-08-07 18:13:41 +0800

    Configure Vite & update Github Actions workflow
    No more regex for rewriting CDN URLs XD

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,43 @@
-import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["@emotion"],
+import type { UserConfig } from "vite";
+
+const base = "/qieyun-autoderiver/";
+
+export function makeConfig(assetLocation = base) {
+  if (!assetLocation.endsWith("/")) {
+    assetLocation += "/";
+  }
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: ["@emotion"],
+        },
+      }),
+    ],
+    base,
+    define: {
+      // For showing the version number on the page
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
+    build: {
+      chunkSizeWarningLimit: 8192,
+      outDir: "build",
+    },
+    experimental: {
+      renderBuiltUrl(filename, type) {
+        //console.log("#[", filename, type, "]#");
+        if (type.hostId === "index.html") {
+          // Rewrite asset URL in index.html
+          return assetLocation + filename;
+        } else {
+          // Use relative paths in other assets
+          return { relative: true };
+        }
       },
-    }),
-  ],
-  base: "/qieyun-autoderiver/",
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-  },
-  build: {
-    chunkSizeWarningLimit: 10240,
-    outDir: "build",
-  },
-});
+    },
+  } satisfies UserConfig;
+}
+
+export default makeConfig();
---
+++ vite.cos.config.ts
@@ -0,0 +1,4 @@
+import { makeConfig } from "./vite.config";
+
+// Same as base config but prefix each asset path in index.html with this location
+export default makeConfig("https://qieyun-autoderiver-1305783649.file.myqcloud.com/");
