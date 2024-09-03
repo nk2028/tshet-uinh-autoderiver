7f0c0c0eb81ec559951e2f6dd1f1104a3f6efee0
Ayaka <ayx@google.com>
2024-09-04 00:59:10 +0100

    Try fix deployment

--- vite.cos.config.ts
+++ vite.cos.config.ts
@@ -1,4 +1,4 @@
 import { makeConfig } from "./vite.config";
 
 // Same as base config but prefix each asset path in index.html with this location
-export default makeConfig("https://qieyun-autoderiver-1305783649.file.myqcloud.com/");
+export default makeConfig("https://autoderiver-1305783649.cos.accelerate.myqcloud.com/");
