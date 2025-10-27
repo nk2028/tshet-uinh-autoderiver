d2d62cc0b6480aa31c4f0872e64592361b1bc6ce
graphemecluster <graphemecluster@gmail.com>
2025-10-27 08:00:53 +0800

    Fix: Incorrect path to translation files in production build

--- src/i18n.ts
+++ src/i18n.ts
@@ -14,7 +14,7 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-      loadPath: "/tshet-uinh-autoderiver/locales/{{lng}}/translation.json",
+      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/translation.json`,
     },
   });
 
