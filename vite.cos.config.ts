114dcd8712174c054d0620acc44c6cb12050b399
Ayaka <ayaka@mail.shn.hk>
2026-02-17 22:23:00 +0100

    Update COS bucket URL

--- vite.cos.config.ts
+++ vite.cos.config.ts
@@ -1,4 +1,4 @@
 import { makeConfig } from "./vite.config";
 
 // Same as base config but prefix each asset path in index.html with this location
-export default makeConfig("https://autoderiver-1305783649.cos.accelerate.myqcloud.com/");
+export default makeConfig("https://nk2028-1305783649.file.myqcloud.com/tshet-uinh-autoderiver/");
